f86f54aec6e9d5f25337b52a2d230b1427292a56
Babin Oleg <babinoleg@mail.ru>
2016-11-07 17:19:11 +0300

    small style changes

--- project/src/core/models.py
+++ project/src/core/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import AbstractUser, AbstractBaseUser
 #https://docs.djangoproject.com/en/1.10/topics/auth/customizing/
 
 class User(AbstractUser):
-    avatar = models.ImageField(u'фото профиля',upload_to='avatars', blank=True, null=True)
+    avatar = models.ImageField(u'фото профиля',upload_to='avatars', blank=False, default=u'avatars/default-avatar.jpg')
     first_name = models.CharField(u'имя', max_length=30, blank=False)
     last_name = models.CharField(u'фамилия', max_length=30, blank=True)
     email = models.EmailField(u'e-mail', blank=False, unique=True)
